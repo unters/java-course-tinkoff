01da9d88582f1d5cd00802106cadfa978eb209b7
Artem Gromakov <gromakov.art3m@gmail.com>
2023-10-04 20:11:55 +0300

    Added solution and tests to task 1.

---
+++ src/main/java/edu/hw1/Task1.java
@@ -0,0 +1,47 @@
+package edu.hw1;
+
+import java.math.BigInteger;
+
+public class Task1 {
+    public static BigInteger minutesToSeconds(String s) {
+        if (s == null)
+            return new BigInteger("-1");
+
+        int delimiterIndex = s.indexOf(':');
+        /* If no ore multiple delimiters exist - return -1.  */
+        if (delimiterIndex == -1 || delimiterIndex != s.lastIndexOf(':')) {
+            return new BigInteger("-1");
+        }
+
+        /* If string starts or ends with delimiter - return -1.  */
+        if (delimiterIndex == 0 || delimiterIndex == s.length() - 1) {
+            return new BigInteger("-1");
+        }
+
+        /* There must be exactly 2 characters after the delimiter.  */
+        if (s.length() - delimiterIndex != 3) {
+            return new BigInteger("-1");
+        }
+
+        /* If string contains any characters except for digits and colon - return -1.  */
+        for (char c : s.toCharArray()) {
+            if (!(Character.isDigit(c) || c == ':')) {
+                return new BigInteger("-1");
+            }
+        }
+
+        BigInteger minutes = new BigInteger(s.substring(0, delimiterIndex));
+        /* Minutes cannot be negative.  */
+        if (minutes.compareTo(new BigInteger("0")) < 0) {
+            return new BigInteger("-1");
+        }
+
+        BigInteger seconds = new BigInteger(s.substring(delimiterIndex + 1));
+        /* Seconds must range from 0 to 59 inclusive.  */
+        if (seconds.compareTo(new BigInteger("0")) < 0 || seconds.compareTo(new BigInteger("60")) >= 0) {
+            return new BigInteger("-1");
+        }
+
+        return minutes.multiply(new BigInteger("60")).add(seconds);
+    }
+}
---
+++ src/test/java/edu/hw1/Task1Test.java
@@ -0,0 +1,93 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.math.BigInteger;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task1Test {
+    @Test void minutesToSeconds_InputContainsZeroOrMoreThanOneDelimiter_ReturnMinusOne() {
+        // given
+        String[] inputStrings = {"124356", "123321:59:01"};
+
+        BigInteger expectedAnswer = new BigInteger("-1");
+        for (String s : inputStrings) {
+            // when
+            BigInteger actualAnswer = Task1.minutesToSeconds(s);
+
+            // then
+            assertThat(actualAnswer).isEqualTo(expectedAnswer);
+        }
+    }
+
+    @Test void minutesToSeconds_InputContainsNonDigitCharactersExceptForColon_ReturnMinusOne() {
+        // given
+        String[] inputStrings = {"ab:20", "01:al", "a9:b8", "ab:cd", "Hello World!"};
+
+        BigInteger expectedAnswer = new BigInteger("-1");
+        for (String s : inputStrings) {
+            // when
+            BigInteger actualAnswer = Task1.minutesToSeconds(s);
+
+            // then
+            assertThat(actualAnswer).isEqualTo(expectedAnswer);
+        }
+    }
+
+    @Test void minutesToSeconds_InputContainsNotTwoCharactersAfterColon_ReturnMinusOne() {
+        // given
+        String[] inputStrings = {"123:0", "246:9", "789:010", "456:101"};
+
+        BigInteger expectedAnswer = new BigInteger("-1");
+        for (String s : inputStrings) {
+            // when
+            BigInteger actualAnswer = Task1.minutesToSeconds(s);
+
+            // then
+            assertThat(actualAnswer).isEqualTo(expectedAnswer);
+        }
+    }
+
+    @Test void minutesToSeconds_InputContainsNoMinutesOrSeconds_ReturnMinusOne() {
+        // given
+        String[] inputStrings = {":00", "12:", ":"};
+
+        BigInteger expectedAnswer = new BigInteger("-1");
+        for (String s : inputStrings) {
+            // when
+            BigInteger actualAnswer = Task1.minutesToSeconds(s);
+
+            // then
+            assertThat(actualAnswer).isEqualTo(expectedAnswer);
+        }
+    }
+
+    @Test void minutesToSeconds_SecondsValueInInputIsGreaterOrEqualThan60_ReturnMinusOne() {
+        // given
+        String[] inputStrings = {"123:60", "123:123"};
+
+        BigInteger expectedAnswer = new BigInteger("-1");
+        for (String s : inputStrings) {
+            // when
+            BigInteger actualAnswer = Task1.minutesToSeconds(s);
+
+            // then
+            assertThat(actualAnswer).isEqualTo(expectedAnswer);
+        }
+    }
+
+    @Test void minutesToSeconds_InputIsValid_ReturnExpectedValue() {
+        // given
+        String[] inputStrings = {"00:00", "00:30", "01:30", "10:00", "1000000000000000000000000:24"};
+
+        String[] expectedAnswers = {"0", "30", "90", "600", "60000000000000000000000024"};
+        for (int i = 0; i < inputStrings.length; ++i) {
+            // when
+            BigInteger actualAnswer = Task1.minutesToSeconds(inputStrings[i]);
+            BigInteger expectedAnswer = new BigInteger(expectedAnswers[i]);
+
+            // then
+            assertThat(actualAnswer).isEqualTo(expectedAnswer);
+        }
+    }
+}
