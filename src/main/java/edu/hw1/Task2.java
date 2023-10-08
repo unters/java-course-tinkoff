0e7fc6aa4efd680ee92420599089d899ecb32637
Artem Gromakov <gromakov.art3m@gmail.com>
2023-10-08 21:58:15 +0300

    Modified solution to task 2
    Created a 'private static final int BASE` variable.

--- src/main/java/edu/hw1/Task2.java
+++ src/main/java/edu/hw1/Task2.java
@@ -1,7 +1,8 @@
 package edu.hw1;
 
 public class Task2 {
-    @SuppressWarnings("MagicNumber")
+    private static final int BASE = 10;
+
     public static int countDigits(int value) {
         if (value == 0) {
             return 1;
@@ -11,7 +12,7 @@ public class Task2 {
         int temp = value;
         while (temp != 0) {
             ++count;
-            temp /= 10;
+            temp /= BASE;
         }
 
         return count;
