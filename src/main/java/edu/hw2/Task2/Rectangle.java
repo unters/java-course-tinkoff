8e0f8589e9926ebcb317b2e0bc8bca09c61c6d13
Artem Gromakov <gromakov.art3m@gmail.com>
2023-10-12 00:42:40 +0300

    Modified solution to task 2
    Added checks for width and height being nonnegative.

--- src/main/java/edu/hw2/Task2/Rectangle.java
+++ src/main/java/edu/hw2/Task2/Rectangle.java
@@ -1,15 +1,24 @@
 package edu.hw2.Task2;
 
 public class Rectangle {
+    /* Let's assume that width and height can equal to zero.  */
     protected short width = 0;
     protected short height = 0;
 
-    RectangleProperties setWidth(short width) {
+    RectangleProperties setWidth(short width) throws IllegalArgumentException {
+        if (width < 0) {
+            throw new IllegalArgumentException("width cannot be negative.");
+        }
+
         this.width = width;
         return new RectangleProperties(this.width, this.height);
     }
 
-    RectangleProperties setHeight(short height) {
+    RectangleProperties setHeight(short height) throws IllegalArgumentException {
+        if (width < 0) {
+            throw new IllegalArgumentException("height cannot be negative.");
+        }
+
         this.height = height;
         return new RectangleProperties(this.width, this.height);
     }
--- src/main/java/edu/hw2/Task2/Square.java
+++ src/main/java/edu/hw2/Task2/Square.java
@@ -1,18 +1,18 @@
 package edu.hw2.Task2;
 
 public class Square extends Rectangle {
-    RectangleProperties setSide(short sideLength) {
+    RectangleProperties setSide(short sideLength) throws IllegalArgumentException {
         super.setHeight(sideLength);
         return super.setWidth(sideLength);
     }
 
     @Override
-    RectangleProperties setWidth(short width) {
+    RectangleProperties setWidth(short width) throws IllegalArgumentException {
         return setSide(width);
     }
 
     @Override
-    RectangleProperties setHeight(short height) {
+    RectangleProperties setHeight(short height) throws IllegalArgumentException {
         return setSide(height);
     }
 
