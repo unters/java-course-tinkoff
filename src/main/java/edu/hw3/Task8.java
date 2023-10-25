843f93bd41221e66334c8c3bd3e5494cb3d750ab
Artem Gromakov <gromakov.art3m@gmail.com>
2023-10-25 13:02:41 +0300

    Changed solution to task 8

--- src/main/java/edu/hw3/Task8.java
+++ src/main/java/edu/hw3/Task8.java
@@ -2,35 +2,31 @@ package edu.hw3;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.ListIterator;
 
-/* Task statement asks to implement an iterator that takes a "collection" as a parameter. This wording is contradictory.
- * The first thing that came to my mind was that the BackwardIterator<> must take a Collection<>. The problem is that
- * the Collection<> interface does not specify any ordering at all (in other words you cannot iterate over an
- * arbitrary Collection<> neither forwards nor backwards).  */
 public class Task8 {
+    /* Task statement asks to implement an iterator that takes a "collection" as a parameter. This wording is
+     * contradictory. The first thing that came to my mind was that the BackwardIterator<> must be applicable to an
+     * arbitrary Collection<>. The problem is that the Collection<> interface does not specify any ordering at all (in
+     * other words you cannot iterate over an arbitrary Collection<> neither forwards nor backwards).
+     *
+     * I've decided to implement a fail-fast backwards iterator for List<> as a wrapper over a fail-fast iterator
+     * ListIterator<> provided by List interface.  */
     public static class BackwardIterator<T> implements Iterator<T> {
-        private final List<T> list;
+        private final ListIterator<T> listIterator;
 
         BackwardIterator(List<T> list) {
-            this.list = list;
-            index = list.size() - 1;
+            listIterator = list.listIterator(list.size());
         }
 
         @Override
         public boolean hasNext() {
-            return index >= 0;
+            return listIterator.hasPrevious();
         }
 
         @Override
         public T next() {
-            if (index < 0) {
-                throw new NoSuchElementException();
-            }
-
-            return list.get(index--);
+            return listIterator.previous();
         }
-
-        private int index;
     }
 }
