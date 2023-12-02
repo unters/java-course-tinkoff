86942d4e569f499bfb829b946f4b48f6b807e279
Artem Gromakov <gromakov.art3m@gmail.com>
2023-12-03 00:24:45 +0300

    Added solution (with tests) to task 3 (pt. 1)
    Added single threaded bruteforcer.

---
+++ src/main/java/edu/hw8/task3/SingleThreadedPasswordBruteforcer.java
@@ -0,0 +1,62 @@
+package edu.hw8.task3;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
+
+@UtilityClass
+public class SingleThreadedPasswordBruteforcer {
+    private static final int HASH_RADIX = 16;
+    private static final int HASH_LENGTH = 32;
+    private static final String HASHING_ALGORITHM = "MD5";
+
+    public static List<String> bruteforce(@NotNull String passwordHash, int minLength, int maxLength) {
+        if (minLength <= 0) {
+            throw new IllegalArgumentException("minLength must be positive");
+        }
+
+        if (minLength >= maxLength) {
+            throw new IllegalArgumentException("maxLength must be greater than minLength");
+        }
+
+        List<String> potentialPasswords = new ArrayList<>();
+        for (int len = minLength; len < maxLength; ++len) {
+            potentialPasswords.addAll(bruteforce(passwordHash, len));
+        }
+        return potentialPasswords;
+    }
+
+    @SneakyThrows(NoSuchAlgorithmException.class)
+    public static List<String> bruteforce(@NotNull String passwordHash, int expectedLength) {
+        if (expectedLength <= 0) {
+            throw new IllegalArgumentException("expectedLength must be positive");
+        }
+
+        MessageDigest messageDigest = MessageDigest.getInstance(HASHING_ALGORITHM);
+        List<String> passwords = new ArrayList<>();
+        PasswordIterator it = new PasswordIterator(expectedLength);
+        while (it.hasNext()) {
+            String nextPassword = it.next();
+            if (passwordHash.equals(getPasswordHash(nextPassword, messageDigest))) {
+                passwords.add(nextPassword);
+            }
+        }
+        return passwords;
+    }
+
+    private static String getPasswordHash(String password, MessageDigest messageDigest) {
+        byte[] digest = messageDigest.digest(password.getBytes());
+        BigInteger bigInteger = new BigInteger(1, digest);
+        String hash = bigInteger.toString(HASH_RADIX);
+        /* Add trailing zeroes to ensure length of 32 characters.  */
+        while (hash.length() < HASH_LENGTH) {
+            hash = "0" + hash;
+        }
+        return hash;
+    }
+}
---
+++ src/test/java/edu/hw8/SingleThreadedPasswordBruteforcerTest.java
@@ -0,0 +1,84 @@
+package edu.hw8;
+
+import edu.hw8.task3.SingleThreadedPasswordBruteforcer;
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SingleThreadedPasswordBruteforcerTest {
+    @ParameterizedTest
+    @CsvSource({"-1, 1", "0, 1", "1, 1", "10, -10"})
+    void bruteforceMinMaxLengths_NullPasswordHashsGiven_ThrowIllegalArgumentsException(int minLength, int maxLength) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SingleThreadedPasswordBruteforcer.bruteforce(null, minLength, maxLength);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"e10adc3949ba59abbe56e057f20f883e, -1, 10", "e10adc3949ba59abbe56e057f20f883e, 0, 10",
+        "e10adc3949ba59abbe56e057f20f883e, 1, 1", "e10adc3949ba59abbe56e057f20f883e, 1, -1"})
+    void bruteforceMinMaxLengths_InvalidLengthsGiven_ThrowIllegalArgumentsException(
+        String passwordHash,
+        int minLength,
+        int maxLength
+    ) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SingleThreadedPasswordBruteforcer.bruteforce(passwordHash, minLength, maxLength);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1})
+    void bruteforceExpectedLength_NullPasswordHashsGiven_ThrowIllegalArgumentsException(int expectedLength) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SingleThreadedPasswordBruteforcer.bruteforce(null, expectedLength);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"e10adc3949ba59abbe56e057f20f883e, -10", "e10adc3949ba59abbe56e057f20f883e, -1",
+        "e10adc3949ba59abbe56e057f20f883e, 0"})
+    void bruteforceExpectedLength_InvalidLengthGiven_ThrowIllegalArgumentsException(
+        String passwordHash,
+        int expectedLength
+    ) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SingleThreadedPasswordBruteforcer.bruteforce(passwordHash, expectedLength);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"072623e6ba866a6db73228be0af0bf45, 4, ab10", "962012d09b8170d912f0669f6d7d9d07, 4, qwer",
+        "f3abb86bd34cf4d52698f14c0da1dc60, 3, zzz"})
+    void bruteforce_ValidAssumptionAboutLengthHasBeenMade_ReturnedListContainsActualPassword(
+        String passwordHash,
+        int expectedLength,
+        String actualPassword
+    ) {
+        // when
+        List<String> potentialPasswords = SingleThreadedPasswordBruteforcer.bruteforce(passwordHash, expectedLength);
+
+        // then
+        assertThat(potentialPasswords.contains(actualPassword)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"072623e6ba866a6db73228be0af0bf45, 3, 5, ab10", "962012d09b8170d912f0669f6d7d9d07, 3, 5, qwer",
+        "f3abb86bd34cf4d52698f14c0da1dc60, 3, 5, zzz"})
+    void bruteforce_ValidAssumptionAboutLengthRangeHasBeenMade_ReturnedListContainsActualPassword(
+        String passwordHash,
+        int minLength,
+        int maxLength,
+        String actualPassword
+    ) {
+        // when
+        List<String> potentialPasswords =
+            SingleThreadedPasswordBruteforcer.bruteforce(passwordHash, minLength, maxLength);
+
+        // then
+        assertThat(potentialPasswords.contains(actualPassword)).isTrue();
+    }
+}
