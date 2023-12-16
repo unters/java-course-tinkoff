6a8a27f321a3dfd88966abf1c2540a3618a68b1a
Artem Gromakov <gromakov.art3m@gmail.com>
2023-12-17 00:06:38 +0300

    Added solution to task 2 (pt. 2)
    Added simple persistence. Data gets saved, but is not downloaded when a new proxy object is created.

--- src/main/java/edu/hw10/task2/CacheProxy.java
+++ src/main/java/edu/hw10/task2/CacheProxy.java
@@ -1,29 +1,37 @@
 package edu.hw10.task2;
 
+import java.io.File;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class CacheProxy implements InvocationHandler {
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private static final Path STORAGE =
+        Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "edu", "persistence");
+
     private final Object targetObject;
     private final Map<String, Object> inMemoryCache = new HashMap<>();
+    private final Path storageFilepath;
 
-    public CacheProxy(Object object) {
-        this.targetObject = object;
-    }
-
-    public static <T> T create(T obj, Class<?> objectClass) {
+    public static <T> T create(T object, Class<?> objectClass) {
         return (T) Proxy.newProxyInstance(
             objectClass.getClassLoader(),
             objectClass.getInterfaces(),
-            new CacheProxy(obj)
+            new CacheProxy(object)
         );
     }
 
@@ -38,10 +46,20 @@ public class CacheProxy implements InvocationHandler {
             } else {
                 Object result = method.invoke(targetObject, args);
                 inMemoryCache.put(key, result);
+                if (cacheAnnotation.persist()) {
+                    Files.write(storageFilepath, (key + " : " + result + "\n").getBytes(), StandardOpenOption.APPEND);
+                }
                 return result;
             }
         }
 
         return method.invoke(targetObject, args);
+    }
+
+    @SneakyThrows
+    private CacheProxy(Object object) {
+        this.targetObject = object;
+        storageFilepath = Paths.get(STORAGE.toString(), UUID.randomUUID() + ".cache");
+        Files.createFile(storageFilepath);
     }
 }
